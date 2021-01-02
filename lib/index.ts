c8e9433326025e45ed93d84fefe8e15cddb7d561
Alex Bailey <alex.john.bailey@gmail.com>
2021-01-02 19:35:44 +0000

    Initial implementation.

--- lib/index.ts
+++ lib/index.ts
@@ -1,15 +1,31 @@
 import { Construct } from '@aws-cdk/core';
+import { Rule, IRuleTarget } from '@aws-cdk/aws-events';
 
-export interface MyConstructProps {
-  // Define construct properties here
+export interface ECRScanMonitorProps {
+  target: IRuleTarget;
 }
 
-export class MyConstruct extends Construct {
+export class ECRScanMonitor extends Construct {
 
-  constructor(scope: Construct, id: string, props: MyConstructProps = {}) {
+  public rule: Rule;
+
+  constructor(scope: Construct, id: string, props: ECRScanMonitorProps) {
     super(scope, id);
 
-    // Define construct contents here
+    this.rule = new Rule(this, 'ecr-image-vulnerability-notification-rule', {
+      ruleName: 'ecr-image-vulnerability-notification-rule',
+      description: 'ECR scan monitor that triggers when a vulnerability is detected.',
+      enabled: true,
+      eventBus: undefined, // uses account default event bus
+      targets: [props.target],
+      eventPattern: {
+        detailType: ['ECR Image Scan'],
+        source: ['aws.ecr'],
+        detail: {
+          'finding-severity-counts': [{'exists': true}]
+        }
+      }
+    });
   }
 
 }
--- test/src.test.ts
+++ test/src.test.ts
@@ -1,15 +1,33 @@
-import { expect as expectCDK, countResources } from '@aws-cdk/assert';
+import { expect as expectCDK, countResources, haveResourceLike } from '@aws-cdk/assert';
 import { App, Stack } from "@aws-cdk/core";
-import { MyConstruct } from '../lib/index';
+import { Topic } from '@aws-cdk/aws-sns';
+import { SnsTopic } from '@aws-cdk/aws-events-targets';
+import { ECRScanMonitor } from '../lib';
 
-/*
- * Example test 
- */
-test('SNS Topic Not Created', () => {
+// TODO: It would be nice to test the event output is the target input but it's a pain without using logical ID's
+test('The event rule is created correctly', () => {
   const app = new App();
   const stack = new Stack(app, "TestStack");
 
-  new MyConstruct(stack, 'MyTestConstruct');
+  let testSNSTopic = new Topic(stack, 'test-topic', {
+    topicName: 'ecr-image-vulnerability'
+  });
 
-  expectCDK(stack).to(countResources("AWS::SNS::Topic",0));
+  new ECRScanMonitor(stack, 'MyTestConstruct', {
+    target: new SnsTopic(testSNSTopic)
+  });
+
+  expectCDK(stack).to(countResources("AWS::Events::Rule",1));
+  expectCDK(stack).to(haveResourceLike("AWS::Events::Rule",{
+    Name: 'ecr-image-vulnerability-notification-rule',
+    Description: 'ECR scan monitor that triggers when a vulnerability is detected.',
+    State: 'ENABLED',
+    EventPattern: {
+      source: ['aws.ecr'],
+      'detail-type': ['ECR Image Scan'],
+      detail: {
+        'finding-severity-counts': [ { 'exists': true } ]
+      }
+    }
+  }));
 });
