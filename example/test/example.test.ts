e11b8d21e52bd3fe3b1f603c1739363a3f5b98e1
Alex Bailey <alex.john.bailey@gmail.com>
2021-01-04 23:06:41 +0000

    Remove stupid example folder

--- example/test/example.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import { App } from '@aws-cdk/core';
-import * as Example from '../lib/example-stack';
-
-test('Empty Stack', () => {
-    const app = new App();
-    const stack = new Example.ExampleStack(app, 'MyTestStack');
-
-});
